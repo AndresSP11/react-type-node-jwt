02c7e0db338c4c97c0f4de56ef6debc581211b8e
AndresSP11 <andres.salazar.p@uni.pe>
2024-09-19 22:52:57 -0500

    mandando

---
+++ Proyecto02/calculadora-propinas/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <header className=" bg-teal-400 py-5">
+        <h1 className=" text-center text-4xl font-semibold">Calculadora de Propinas y consumo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
